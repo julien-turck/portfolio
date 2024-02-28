951eca800691c8a1d71bab0960362ca9b6e78138
julienturck <julien.turck.tic@gmail.com>
2024-02-28 10:36:01 +0100

    finich script for all modals

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,22 +1,61 @@
-/*
-let openBtn = document.getElementById("openModal");
+let btn1 = document.getElementById("button1")
+btn1.addEventListener('click', function(){
+    const modal = document.getElementById("modal1");
+    modal.classList.add("active")
+    const modalTrigger = document.querySelectorAll(".modal-trigger");
 
-openBtn.addEventListener('click', function(){
-   document.getElementById("modal").style.visibility = "visible";
+    modalTrigger.forEach(trigger => trigger.addEventListener("click", toggleModal))
+
+    function toggleModal(){
+        modal.classList.remove("active")
+    }
 });
 
-let closeBtn = document.getElementById("closeModal");
+let btn2 = document.getElementById("button2")
+btn2.addEventListener('click', function(){
+    const modal = document.getElementById("modal2");
+    modal.classList.add("active")
+    const modalTrigger = document.querySelectorAll(".modal-trigger");
 
-closeBtn.addEventListener('click', function(){
-   document.getElementById("modal").style.visibility = "hidden";
+    modalTrigger.forEach(trigger => trigger.addEventListener("click", toggleModal))
+
+    function toggleModal(){
+        modal.classList.remove("active")
+    }
 });
-*/
 
-const modal = document.querySelector(".modal");
+let btn3 = document.getElementById("button3")
+btn3.addEventListener('click', function(){
+    const modal = document.getElementById("modal3");
+    modal.classList.add("active")
+    const modalTrigger = document.querySelectorAll(".modal-trigger");
+
+    modalTrigger.forEach(trigger => trigger.addEventListener("click", toggleModal))
+
+    function toggleModal(){
+        modal.classList.remove("active")
+    }
+});
+
+let btn4 = document.getElementById("button4")
+btn4.addEventListener('click', function(){
+    const modal = document.getElementById("modal4");
+    modal.classList.add("active")
+    const modalTrigger = document.querySelectorAll(".modal-trigger");
+
+    modalTrigger.forEach(trigger => trigger.addEventListener("click", toggleModal))
+
+    function toggleModal(){
+        modal.classList.remove("active")
+    }
+});
+
+/*const modal = document.getElementById("modal1");
 const modalTrigger = document.querySelectorAll(".modal-trigger");
 
 modalTrigger.forEach(trigger => trigger.addEventListener("click", toggleModal))
 
 function toggleModal(){
     modal.classList.toggle("active")
-}
+}*/
+
